cb9afa6d2ba9cdd5e898c74ccb3fd8f4c9b2cb4c
Kyle Thornburgh <thornburghkyle@gmail.com>
2023-10-31 13:35:14 -0400

    Convert code to be more Rusty.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,11 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess a number between 1 and 100! Please enter your guess as an integer.");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is {secret_number}");
-
-    println!("Please input your guess as an integer between 1 and 100:");
 
     loop {
         let mut guess = String::new();
@@ -17,23 +14,21 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess = guess.trim().parse::<i32>();
-        if guess.is_err() {
-            println!("Your guess is not a valid number. Try again.");
-            continue;
-        }
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Your guess is not a valid number. Try again.");
+                continue;
+            }
+        };
 
-        let guess: i32 = guess.expect("Please enter a number");
-
-        if guess == secret_number {
-            println!("You win!");
-            break;
-        } else if guess < secret_number {
-            println!("Too low. Try again.");
-        } else if guess > secret_number {
-            println!("Too high. Try again.");
-        } else if guess != secret_number {
-            println!("Something went wrong. Quit trying to break the game! Try again, following the rules this time.");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low. Try again."),
+            Ordering::Greater => println!("Too high. Try again."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
