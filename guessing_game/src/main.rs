092a5fdb99a879e14a927b01f7d24c9b4ab6fb89
Kyle Thornburgh <thornburghkyle@gmail.com>
2023-10-31 13:08:44 -0400

    Switch from while loop to generic loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,32 +10,30 @@ fn main() {
 
     println!("Please input your guess as an integer between 1 and 100:");
 
-    let mut guess_int: i32 = -1;
-
-    while guess_int != secret_number {
+    loop {
         let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let result = guess.trim().parse::<i32>();
-        if result.is_err() {
+        let guess = guess.trim().parse::<i32>();
+        if guess.is_err() {
             println!("Your guess is not a valid number. Try again.");
             continue;
         }
 
-        let guess: i32 = result.expect("Please enter a number");
+        let guess: i32 = guess.expect("Please enter a number");
 
-        guess_int = guess;
-
-        if guess_int < secret_number {
+        if guess == secret_number {
+            println!("You win!");
+            break;
+        } else if guess < secret_number {
             println!("Too low. Try again.");
-        } else if guess_int > secret_number {
+        } else if guess > secret_number {
             println!("Too high. Try again.");
-        } else if guess_int != secret_number {
+        } else if guess != secret_number {
             println!("Something went wrong. Quit trying to break the game! Try again, following the rules this time.");
         }
     }
-
-    println!("You win!");
 }
